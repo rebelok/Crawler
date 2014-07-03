a4011d2df9f9f6a55f1b14b0b55424ce44d50295
rebelok <vdovichencko@gmail.com>
2014-07-04 02:49:25 +0400

    Init

---
+++ Crawler.Data/Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crawler.Data
+{
+    public class Context : DbContext
+    {
+        public Context()
+            : base()
+        {
+
+        }
+        public DbSet<Game> Games { get; set; }
+    }
+}
---
+++ Crawler.Data/GameDescription.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crawler.Data
+{
+    public enum GameType
+    {
+        Action,
+        Arcade,
+        RPG,
+        Strategy,
+        Racing,
+        Logic,
+        Table,
+        Sport,
+        Study,
+        Education,
+        Other
+
+    }
+    public class Game
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string OS { get; set; }
+        public string MarketUrl { get; set; }
+        public string CoverlUrl { get; set; }
+
+        public GameType Type { get; set; }
+        
+    }
+}
